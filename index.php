7aa48db0c9f53d84080f3e6530e8b3433bbe7d9a
Bil <bilg507@gmail.com>
2017-02-22 17:01:35 +0545

    First Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Laxmi | Mart</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+</head
+<body>
+<p>Laxmi Mart</p>
+</body>
+</html>
